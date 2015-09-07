29d1182e942f509bc268c96ed49911c4eefc79d5
Emma Elliott <elliott_e@lynchburg.edu>
2015-09-07 19:19:57 -0400

    Create plane.h

---
+++ plane.h
@@ -0,0 +1,54 @@
+// Emma Elliott
+// With code from Will Foley, Cory Budka
+// Fall 2015
+// This file is for the class Plane
+
+#pragma once
+#include <irrlicht.h>
+#include <string>
+#include <vector>
+#include "Object.h"
+using namespace irr;
+
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+#ifdef _IRR_WINDOWS_
+#pragma comment(lib, "Irrlicht.lib")
+#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+
+class Plane : public Object
+{
+public:
+	// The default constructors inherits from Object
+	Plane() : Object() {};
+	Plane(const Plane& other) : Object(other) {};
+	Plane(const Object& other) : Object(other) {};
+
+	bool correctMeshTextureSound()
+	{
+		// call the three access functions
+		// check to see if the mesh, the sound, and the texture are correct for class
+	}
+
+	void physics()
+	{
+		// Functions to create the physics for the objects
+	}
+
+
+	LEVEL_OF_INTERACTIVITY level_ = ID_INTERACTABLE;
+	void onCollision(Object* object)
+	{
+		//if object.getInteractivity == ID_INTERACTABLE
+		//plane location and rotation is reset
+		//this means the plane hit a landing pad
+		//else if object.getIteractivity != ID_AQUIRABLE (if plane did not hit a powerup)
+		//plane is destroyed
+	}
+	
+};
