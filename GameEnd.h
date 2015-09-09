79140425b65317e35e3e3d87c70924163eb92c32
Cory Budka <budka_c@lynchburg.edu>
2015-09-08 21:06:05 -0400

    Create GameEnd.h

---
+++ GameEnd.h
@@ -0,0 +1,22 @@
+//Cory Budka
+//This is the GameEnd class which will determine if the user has won or lost
+
+#pragma once 
+#include <irrlicht.h> 
+#include "Object.h"
+#include "LandingPad.h"
+
+Class GameEnd
+{
+public:
+   GameEnd();
+   GameEnd(const GameEnd& other);
+   void didYouWin (Object whatYouCollidedWith);
+
+void didYouWin (Object whatYouCollidedWith)
+{
+  // if the collision is with the final pad, they WIN
+  // else the collision is ith any other object, they LOSE
+}
+
+};
