3dbdaf275a3975bf5ad752b6fb63d0593e192b71
JoeDavis7 <davis_jl@lynchburg.edu>
2015-10-02 14:33:21 -0400

    hud.h and sound.h

--- Sound.h
+++ Sound.h
@@ -1,52 +1,122 @@
 //Sound.h
-//Joseph Davis
-//Irrklang 3d / 2d sound library
-//•	IrrKlang is a 2d / 3d sound library that has a large range of compatibilities that allows it to be easily implemented into a graphic engine such as irrKlang
-//•	The function engine->Play2D(“musicfile.mp3”, true) will play a supported sound file in the background of the program when ran, it will also loop the sound if true is added to the parameters.
-//	o	Mp3 format is only supported if you include the dll that the program natively provides
-//•	Engine->Play3d(“musicfile”, position(vec3df), true) allows anyone to play a sound in a 3d space at any position that they desire and it also can be looped as well
-//•	The engine also can allow for the sounds to be set to a minimal distance so that you will not hear that particular sound until you get closer to it in the 3d space
-//•	Irrklang does also support the use of chorus, flanger, and etc sound effects within the engine natively.
-//•	Within IrrKlang, you have the option to record your own sounds using microphone or any other input device
-// Sound class may not be needed but here one just in case
+
+#ifndef SOUND
+#define SOUND
+
 
 #include <irrlicht.h>
 #include <irrKlang.h>
-
+#include <iostream>
 #include <string>
+
 
 using namespace irr;
 using namespace irrklang;
 using namespace scene;
 
+using namespace std;
+
 // sound class 
 class Sound
 {
 public:
+
+	Sound();
+	Sound(const std::string& fileName);
+	~Sound();
 	
 	//plays the sound once
 	void playSoundOnce(bool deleteFile = false);
 	
 	// sets sound to play
-	void PlaySound(const char* filename);
+	void playSound();
 
-	// gets the sound
-	const char* getSound() const;
+	void playSound(const std::string& soundLocation);
 
+	void setFileName(const std::string& soundName);
 
-	// gets the distance in a 3d setting
-	void setDistance(f32 minDistance, f32 maxDistance);
+	std::string getFileLocation();
+
+	bool isPlaying();
 
 	// stops the sound from playing
 	void stop();
 
+	void pause();
+
+
 private:
-	f32 MinDistance_;
-	f32 MaxDistance_;
 	irrklang::ISoundEngine* engine_;
-	irrklang::ISound* Sound_;
+	irrklang::ISound* csound_;
+	std::string fileName_;
+	size_t volume_;
+	size_t position_;
 
 
 
 
 };
+
+#endif
+
+Sound::Sound() : volume_(50), position_(0), fileName_(""), csound_(0)
+{
+	engine_ = createIrrKlangDevice();
+
+	if (!engine_)
+	{
+		exit(0);
+	}
+
+}
+Sound::Sound(const std::string& fileName)
+{
+	fileName_ = fileName;
+}
+Sound::~Sound()
+{
+	engine_->drop();
+	csound_->drop();
+}
+void Sound::playSound()
+{
+	csound_ = engine_->play2D(fileName_.c_str(), false, false, true);
+}
+
+void Sound::playSound(const std::string& soundLocation)
+{
+	setFileName(soundLocation);
+	csound_ = engine_->play2D(fileName_.c_str(), false, false, true);
+}
+
+
+
+
+std::string Sound::getFileLocation()
+{
+
+	return fileName_;
+}
+
+void Sound::setFileName(const std::string& soundName)
+{
+
+	fileName_ = soundName;
+}
+
+
+bool Sound::isPlaying()
+{
+	return !csound_->isFinished();
+}
+
+void Sound::stop()
+{
+	 engine_->stopAllSounds();
+}
+
+
+void Sound::pause()
+{
+	engine_->setAllSoundsPaused();
+}
